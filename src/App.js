444850a70cf563f09b53cdb516528c4f43c439fb
Nicholas Bennet <bennet08121996nicholas@gmail.com>
2021-04-20 02:09:17 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,250 @@
+import './App.css';
+
+import { Navbar, Nav, Container, Row, Col, Button } from 'react-bootstrap';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import {
+	faEnvelope,
+	faPhoneSquare,
+	faFileDownload,
+} from '@fortawesome/free-solid-svg-icons';
+
+import profile from './profile.jpg';
+
+import { education, experience, projects, skills } from './resume';
+
+import { Education, Experience, Projects, Skills } from './Components';
+
+library.add(fab, faEnvelope, faPhoneSquare, faFileDownload);
+
+function App() {
+	return (
+		<div className="App">
+			<Navbar
+				bg="dark"
+				expand="lg"
+				variant="dark"
+				fixed="top"
+				collapseOnSelect="True"
+			>
+				<Navbar.Brand href="#home">
+					<span className="nicholas-brand">NICHOLAS</span>BENNET
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="justify-content-center">
+						<Nav.Link href="#about">About</Nav.Link>
+						<Nav.Link href="#education">Education</Nav.Link>
+						<Nav.Link href="#experience">Experience</Nav.Link>
+						<Nav.Link href="#projects">Projects</Nav.Link>
+						<Nav.Link href="#skills">Skills</Nav.Link>
+						<Nav.Link href="/resume.pdf" target="_blank">
+							Resume
+						</Nav.Link>
+						<Nav.Link href="#contact">Contact</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+			<div className="header" id="home">
+				<div className="inner">
+					<img src={profile} alt="Profile" className="profile-pic"></img>
+					<h1 className="inner-text">
+						<span role="img" aria-label="Hi">
+							👋
+						</span>
+						&nbsp;I'M NICHOLAS
+					</h1>
+					<span className="inner-text">
+						<a
+							href="https://github.com/nicholasbennet"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FontAwesomeIcon icon={['fab', 'github']} />
+						</a>
+						<a
+							href="https://www.linkedin.com/in/nicholasbennet/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FontAwesomeIcon icon={['fab', 'linkedin']} />
+						</a>
+						<a
+							href="mailto:hi@nicholasben.net"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<FontAwesomeIcon icon="envelope" />
+						</a>
+						<a href="tel:+13479710488" target="_blank" rel="noreferrer">
+							<FontAwesomeIcon icon="phone-square" />
+						</a>
+					</span>
+				</div>
+			</div>
+			<Container id="about">
+				<Row>
+					<Col className="heading">
+						<h1>About Me</h1>
+					</Col>
+				</Row>
+				<Row>
+					<Col className="about-text">
+						<p>
+							I'm Nicholas Bennet, a 24 year old Computer Engineer who will be
+							graduating with a Master's in Computer Engineering from New York
+							University on May 19th, 2021. I am excited about everything
+							software, networking and security related and love to use my
+							knowledge and careful research to help solve real world problems.
+							I am also a self taught Web Developer with a good eye for detail.
+							<br />
+							<br />
+							In my free time you would usually find my tinkering with my
+							Raspberry Pi HomeLab or cycling and exploring the beautiful New
+							York City.
+							<br />
+							<br />
+							Currenty a Sytem Adminiistrator and Cybersecurity Awareness
+							Assistant with the Global Office of Information Security at NYU
+							IT. I work on integrating Cybersecurity Awareness platforms with
+							the existing NYU IT infrastructure and also am part of the
+							discussion on security upgrades for the NYU IT infrastructure.
+						</p>
+					</Col>
+				</Row>
+			</Container>
+			<Container id="education">
+				<Row>
+					<Col className="heading">
+						<h1>Education</h1>
+					</Col>
+				</Row>
+				<Education education={education} />
+			</Container>
+			<Container id="experience">
+				<Row>
+					<Col className="heading">
+						<h1>Experience</h1>
+					</Col>
+				</Row>
+				<Experience experience={experience} />
+			</Container>
+			<Container id="projects">
+				<Row>
+					<Col className="heading">
+						<h1>Projects</h1>
+					</Col>
+				</Row>
+				<Projects projects={projects} />
+			</Container>
+			<Container id="skills">
+				<Row>
+					<Col className="heading">
+						<h1>Technical Skills</h1>
+					</Col>
+				</Row>
+				<Skills skills={skills} />
+			</Container>
+			<Container id="contact">
+				<Row>
+					<Col className="heading">
+						<h1>Contact Me</h1>
+					</Col>
+				</Row>
+				<Row>
+					<Col className="contact-det">
+						<div className="contact-phone">
+							<FontAwesomeIcon icon="phone-square" />
+							&nbsp;
+							<a href="tel:+13479710488" target="_blank" rel="noreferrer">
+								+1-(347)-971-0488
+							</a>
+						</div>
+						<div className="contact-email">
+							<FontAwesomeIcon icon="envelope" />
+							&nbsp;
+							<a
+								href="mailto:hi@nicholasben.net"
+								target="_blank"
+								rel="noreferrer"
+							>
+								hi@nicholasben.net
+							</a>
+							&nbsp;,&nbsp;
+							<a
+								href="mailto:nicholas.bennet@nyu.edu"
+								target="_blank"
+								rel="noreferrer"
+							>
+								nicholas.bennet@nyu.edu
+							</a>
+						</div>
+						<div className="resume-download-btn">
+							<a href="/resume.pdf" download="Resume.pdf">
+								<Button variant="dark">
+									<FontAwesomeIcon icon="file-download" /> Download my Resume
+								</Button>
+							</a>
+						</div>
+
+						<span className="contact-text">
+							<a
+								href="https://github.com/nicholasbennet"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FontAwesomeIcon icon={['fab', 'github']} />
+							</a>
+							<a
+								href="https://www.linkedin.com/in/nicholasbennet/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FontAwesomeIcon icon={['fab', 'linkedin']} />
+							</a>
+							<a
+								href="mailto:hi@nicholasben.net"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FontAwesomeIcon icon="envelope" />
+							</a>
+							<a href="tel:+13479710488" target="_blank" rel="noreferrer">
+								<FontAwesomeIcon icon="phone-square" />
+							</a>
+						</span>
+					</Col>
+				</Row>
+			</Container>
+			<Row className="footer">
+				<Col>
+					<div>
+						Made using{' '}
+						<a
+							href="https://reactjs.org/"
+							target="_blank"
+							rel="noreferrer"
+							className="react"
+						>
+							ReactJS
+						</a>
+						&nbsp;and&nbsp;
+						<a
+							href="https://react-bootstrap.github.io/"
+							target="_blank"
+							rel="noreferrer"
+							className="react"
+						>
+							React Bootstrap
+						</a>
+					</div>
+					<div>&copy; 2021 Nicholas Bennet</div>
+				</Col>
+			</Row>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/resume.js
@@ -0,0 +1,99 @@
+const education = [
+	{
+		img: '/nyu.png',
+		year: '2019 - 2021',
+		location: 'New York, NY',
+		school: 'New York University',
+		degree: 'MS Computer Engineering',
+		gpa: '3.37/4.00 GPA',
+	},
+	{
+		img: '/srm.jpg',
+		year: '2014 - 2018',
+		location: 'Chennai, India',
+		school: 'SRM University',
+		degree: 'BTech Electronics and Communication Engineering',
+	},
+];
+
+const experience = [
+	{
+		img: '/nyuit.png',
+		company: 'NYU IT',
+		year: 'Nov 2019 - Present',
+		location: 'New York, NY',
+		position: 'System Administrator and Security Awareness Assistant',
+		status: 'Part Time',
+	},
+	{
+		img: '/solarify.png',
+		company: 'Solarify',
+		year: 'Jan 2019 - May 2019',
+		location: 'Bengaluru, India',
+		position: 'Web Developer',
+		status: 'Full Time',
+	},
+	{
+		company: 'Newranium Creatives',
+		year: 'Mar 2017 - Mar 2018',
+		location: 'Chennai, India',
+		position: 'Fronted Web Developer',
+		status: 'Part Time',
+	},
+	{
+		company: '42 Labs',
+		year: 'Dec 2015 - Apr 2016',
+		location: 'Chennai, India',
+		position: 'Web Developer',
+		status: 'Part Time',
+	},
+];
+
+const projects = [
+	{
+		name: 'Neural Network Backdoor Removal',
+		tools: 'Python, TensorFlow, Keras, NumPy',
+		year: 'Dec 2020',
+		link: 'https://github.com/nicholasbennet/neural-network-backdoor-removal',
+	},
+	{
+		name: 'Video Call with Audio and Video Filters',
+		tools: 'Python, CV2, PyAudio, NumPy',
+		year: 'Dec 2020',
+		link: 'https://github.com/nicholasbennet/video-call',
+	},
+	{
+		name: 'Gesture Controlled Lock',
+		tools: 'C++, Arduino',
+		year: 'May 20200',
+		link: 'https://github.com/nicholasbennet/Gesture_Unlock',
+	},
+	{
+		name: 'Recipe Randomizer',
+		tools: 'Python, ReactJS, NodeJS',
+		year: 'Apr 2020',
+		link: 'https://github.com/nicholasbennet/random-recipe',
+	},
+];
+
+const skills = [
+	{
+		name: 'Programming',
+		tools: 'C++, Java, MATLAB, Python',
+	},
+	{
+		name: 'Frontend',
+		tools: 'AngularJS, JavaScript, jQuery, ReactJS',
+	},
+	{
+		name: 'Backend',
+		tools: 'GraphQL, MongoDB, NodeJS, SQL',
+	},
+	{
+		name: 'Other Tools',
+		tools:
+			'AWS, Azure, Docker, Google Cloud, Git, Jira, Linux, Selenium, Windows, Tensorflow, OpenCV',
+	},
+];
+
+export { education, experience, projects, skills };
